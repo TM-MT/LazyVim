5c5d61f8f1a398cb50ca88edca5e3862899f8fa2
TM-MT <tm-east@outlook.com>
2025-11-02 13:40:20 +0900

    Add swift support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,22 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "swift" },
+  callback = function()
+    local root_dir = vim.fs.dirname(vim.fs.find({
+      "Package.swift",
+      "*.podspec",
+      ".git",
+    }, { upward = true })[1])
+    local client = vim.lsp.start({
+      name = "sourcekit-lsp",
+      cmd = { "sourcekit-lsp" },
+      root_dir = root_dir,
+    })
+    vim.lsp.buf_attach_client(0, client)
+  end,
+  group = swift_lsp,
+})
