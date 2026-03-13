9199b797775ec00fb33a689239a042c8de4aa0c3
TM-MT <tm-east@outlook.com>
2026-03-13 23:28:03 +0900

    Clipboard sharing for WSL

--- init.lua
+++ init.lua
@@ -1,2 +1,19 @@
+local is_wsl = vim.fn.system("uname -r"):lower():find("microsoft") ~= nil
+if is_wsl then
+  -- WSL Config
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
